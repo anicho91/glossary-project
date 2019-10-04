447c37a3863c245714121660bea602898b4d89af
BarbaraHernandez <bandezanne@gmail.com>
2019-10-04 15:45:28 -0400

    working

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -3,11 +3,33 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @EnableConfigServer
 @SpringBootApplication
+@RequestMapping(value = "/glossary")
 public class ConfigServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+
+	//private ServiceLayer service;
+
+	//generate constructor
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Definition createDefinition(@RequestBody Definition def){
+		//return service.createDefinition(def);
+		return def;
 	}
+
+	@GetMapping(value = "/term/{term}")
+	@ResponseStatus
+	public List<Definition> getDefinitions(@PathVariable String term){
+		//List<Definition> defs = service.getDefinitions(term);
+		//return defs;
+	}
+
+
+
+
 }
---
+++ glossary-service/src/main/java/com/company/glossaryservice/controller/GlossaryServiceController.java
@@ -0,0 +1,7 @@
+package com.company.glossaryservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlossaryServiceController {
+}
