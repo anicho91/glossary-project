ab2f25340aef2f9d2fcd2b724d22633907af09fc
BarbaraHernandez <bandezanne@gmail.com>
2019-10-04 23:59:17 -0400

    rewrote controllers per service layer and view model

--- glossary-service/src/main/java/com/company/glossaryservice/controller/GlossaryServiceController.java
+++ glossary-service/src/main/java/com/company/glossaryservice/controller/GlossaryServiceController.java
@@ -1,7 +1,35 @@
 package com.company.glossaryservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.glossaryservice.dto.GlossaryViewModel;
+import com.company.glossaryservice.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/glossary")
 public class GlossaryServiceController {
+
+    private ServiceLayer service;
+
+    @Autowired
+    public GlossaryServiceController(ServiceLayer service) {
+        this.service = service;
+    }
+
+    //post
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GlossaryViewModel createDefinition(@RequestBody GlossaryViewModel gsvm){
+        return service.addTermDefintion(gsvm);
+    }
+
+    //get
+    @GetMapping(value = "/term/{term}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public GlossaryViewModel getDefinitionsByTerm(@PathVariable String term){
+        return service.getTermDefinitions(term);
+    }
 }
